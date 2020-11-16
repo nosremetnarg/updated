6586c8febccd5a7b1fd925a565f86ad1965fef5c
Grant Emerson <grant.alex.emerson@gmail.com>
2020-11-15 20:04:43 -0600

    Updates

---
+++ precache-manifest.ed645adb2878c85821d020f689190687.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2cfc45a11efefffdd4ba0a3a0e3247",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "fb6de2f7f8f0eda67c60",
+    "url": "/ReactPortfolio/static/css/main.ad0c8798.chunk.css"
+  },
+  {
+    "revision": "8bebf0377224c70e8ad0",
+    "url": "/ReactPortfolio/static/js/2.3e90e5c7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ReactPortfolio/static/js/2.3e90e5c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb6de2f7f8f0eda67c60",
+    "url": "/ReactPortfolio/static/js/main.eb41f13d.chunk.js"
+  },
+  {
+    "revision": "245e88873ef0a2539a5e",
+    "url": "/ReactPortfolio/static/js/runtime-main.ebaa2495.js"
+  },
+  {
+    "revision": "2b8676151edbe1f0918e911f1657d479",
+    "url": "/ReactPortfolio/static/media/0.2b867615.jpg"
+  },
+  {
+    "revision": "356256086387a0458754eeb163465835",
+    "url": "/ReactPortfolio/static/media/0.35625608.jpg"
+  },
+  {
+    "revision": "eb1e5668258262d92592e2e089298c8e",
+    "url": "/ReactPortfolio/static/media/0.eb1e5668.jpg"
+  },
+  {
+    "revision": "c5fd2c3d7cfc9077d37c3ae3e5dab20f",
+    "url": "/ReactPortfolio/static/media/1.c5fd2c3d.jpg"
+  },
+  {
+    "revision": "d52f1f03aa9f0ecc06217dfaf7696da9",
+    "url": "/ReactPortfolio/static/media/1.d52f1f03.jpg"
+  },
+  {
+    "revision": "00048ca33e3a3d57291eba782c7de91f",
+    "url": "/ReactPortfolio/static/media/2.00048ca3.jpg"
+  },
+  {
+    "revision": "ebd2ebb351728dac44ab6e4f4ab67b35",
+    "url": "/ReactPortfolio/static/media/2.ebd2ebb3.jpg"
+  },
+  {
+    "revision": "77af499961a619b603a4ce9fb2a58d20",
+    "url": "/ReactPortfolio/static/media/3.77af4999.jpg"
+  },
+  {
+    "revision": "cc6b4b36ad78c17566c32fa65bbacc7e",
+    "url": "/ReactPortfolio/static/media/4.cc6b4b36.jpg"
+  },
+  {
+    "revision": "a5397e25c1ce8cebeb7f8647bb610c7c",
+    "url": "/ReactPortfolio/static/media/cover-image2.a5397e25.JPG"
+  }
+]);
